3269bf915e689ee25b56ae2a7f58b5910a8b80b4
minwira <111176379+minwira@users.noreply.github.com>
2022-08-18 14:20:12 +0700

    Add files via upload

---
+++ Exercise4_minwira_p.py
@@ -0,0 +1,9 @@
+FE = 75.6
+GB =82.5
+ICS = 85.4
+CP =74.9
+print("--- Your Score ---")
+print("Foundation English :", FE)
+print("General Business :", GB)
+print("Introduction to Computer System :", ICS)
+print("Computer Programing :",CP)
